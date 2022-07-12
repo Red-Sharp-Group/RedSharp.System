8f5f15aa49bc49d8c69fc9f3b55a83ad0ef7eed4
Andrii Kudriavstev <extosis.vt@gmail.com>
2022-07-12 21:44:32 +0300

    A new interface for as a variant of initializing process was added.

---
+++ RedSharp.System/Interfaces/Shared/IFreezable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedSharp.Sys.Interfaces.Shared
+{
+    public interface IFreezable
+    {
+        void Freeze();
+
+        void Unfreeze();
+    }
+
+    public interface IFreezeIndication
+    {
+        bool IsFrozen { get; }
+    }
+}
