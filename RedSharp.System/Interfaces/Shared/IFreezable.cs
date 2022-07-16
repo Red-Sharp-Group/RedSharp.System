4d025ea29aba392c01b0b9b420c9054c1459f5ed
Andrii Kudriavstev <extosis.vt@gmail.com>
2022-07-16 16:41:13 +0300

    A variation of initializable objects was added.

---
+++ RedSharp.System/Interfaces/Shared/IFreezable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedSharp.Sys.Interfaces.Shared
+{
+    /// <summary>
+    /// Another implementation of initializable objects 
+    /// that allows you to do actions and then freeze object state.
+    /// </summary>
+    public interface IFreezable
+    {
+        /// <summary>
+        /// Disallows changing the object state.
+        /// </summary>
+        void Freeze();
+
+        /// <summary>
+        /// Allows changing the object state.
+        /// </summary>
+        /// <remarks>
+        /// Be careful: may throw <see cref="NotSupportedException"/>, usually object only freezable.
+        /// </remarks>
+        void Unfreeze();
+    }
+}
---
+++ RedSharp.System/Interfaces/Shared/IFreezeIndication.cs
@@ -0,0 +1,13 @@
+﻿namespace RedSharp.Sys.Interfaces.Shared
+{
+    /// <summary>
+    /// Uses in a pair with <see cref="IFreezable"/> to indicate current state to others.
+    /// </summary>
+    public interface IFreezeIndication
+    {
+        /// <summary>
+        /// Shows that the object was frozen and cannot be changed or vice versa.
+        /// </summary>
+        bool IsFrozen { get; }
+    }
+}
