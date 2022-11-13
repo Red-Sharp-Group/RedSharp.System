ba6c2acd4b5e4f6a585eb12b64fad1e3771fb6b5
Andrii Kudriavtsev <extosis.vt@gmail.com>
2022-11-13 16:52:59 +0200

    Identity interface was added.

---
+++ RedSharp.System/Interfaces/Shared/IIdentity.cs
@@ -0,0 +1,13 @@
+﻿namespace RedSharp.Sys.Interfaces.Shared
+{
+    /// <summary>
+    /// This interface is need to distinguish one interface implementation from other, in case when you have several of them.
+    /// </summary>
+    public interface IIdentity
+    {
+        /// <summary>
+        /// Any kind of information that is unique for the current realization.
+        /// </summary>
+        string Identity { get; }
+    }
+}
