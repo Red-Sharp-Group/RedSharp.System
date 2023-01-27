90762aa9e007952c87ce5ad801f76d04f248d751
Andrii Kudriavtsev <extosis.vt@gmail.com>
2023-01-27 22:30:24 +0200

    Grammar error was fixed.

--- RedSharp.System/Abstracts/DisposableBase.cs
+++ RedSharp.System/Abstracts/DisposableBase.cs
@@ -14,7 +14,7 @@ namespace RedSharp.Sys.Abstracts
 
         ~DisposableBase()
         {
-            SafeDisposed(false);
+            SafeDispose(false);
         }
 
         /// <inheritdoc/>
@@ -26,7 +26,7 @@ namespace RedSharp.Sys.Abstracts
             if (IsDisposed)
                 return;
 
-            SafeDisposed(true);
+            SafeDispose(true);
 
             GC.SuppressFinalize(this);
 
@@ -40,7 +40,7 @@ namespace RedSharp.Sys.Abstracts
         /// Do not change the method signature.
         /// </remarks>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private void SafeDisposed(bool manual)
+        private void SafeDispose(bool manual)
         {
             try
             {
